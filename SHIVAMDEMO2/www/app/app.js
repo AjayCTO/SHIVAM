3704b6c0525869aa5f8c728b2decdeb9e08e2719
Ajay Paliwal <ajaypaliwal.cto@outlook.com>
2016-07-06 14:58:01 +0530

    update
    update

---
+++ SHIVAMDEMO2/www/app/app.js
@@ -0,0 +1,74 @@
+﻿
+var app = angular.module('AngularAuthApp', ['ngRoute', 'ngSanitize', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "app/views/login.html"
+    });
+
+   
+
+    $routeProvider.when("/refresh", {
+        controller: "refreshController",
+        templateUrl: "app/views/refresh.html"
+    });
+
+    $routeProvider.when("/tokens", {
+     controller: "tokensManagerController",
+     templateUrl: "app/views/tokens.html"
+    });
+
+    $routeProvider.when("/associate", {
+       controller: "associateController",
+      templateUrl: "app/views/associate.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/login" });
+
+});
+
+//var serviceBase = 'http://localhost:7440/API/ClearlyInventoryAPI.svc/';
+//var serviceBaseUrl = 'http://localhost:7440/';
+var serviceBaseUrl = 'https://test.inventory4.com/';
+var serviceBase = 'https://test.inventory4.com/API/ClearlyInventoryAPI.svc/';
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'ngAuthApp'
+});
+
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+// factory for all messages 
+app.factory('log', function () {
+    toastr.options = {
+        closeButton: true,
+        positionClass: 'toast-top-right',
+    };
+    return {
+        success: function (text) {
+            toastr.success(text, "Success");
+        },
+        error: function (text) {
+            toastr.error(text, "Error");
+        },
+        info: function (text) {
+            toastr.info(text, "Info");
+        },
+        warning: function (text) {
+            toastr.warning(text, "Warning");
+        },
+    };
+});
+
+
+ 
---
+++ SHIVAMDEMO2/www/app/controllers/indexController.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+app.controller('indexController', ['$scope', 'localStorageService', 'authService', '$location', 'log', function ($scope, localStorageService, authService, $location, log) {
+  
+    $scope.logOut = function () {
+      
+       
+        
+        authService.logOut();
+        $location.path('/login');
+    }
+
+
+
+
+}]);
